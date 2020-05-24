f7aab6436efc7428d424474a8f7f5899f5b0396b
marui <marui@ut.cn>
2020-05-24 19:06:38 +0800

    上传服务模板

--- src/template/ServiceTemplate.ts
+++ src/template/ServiceTemplate.ts
@@ -11,12 +11,18 @@ class ServiceTemplate implements ServiceTemplateInterface {
   constructor(config: ServiceOptions){
     this.config = config;
   }
+  getTemplate(){
+    
+  }
+  getChunkCode(){
+    return `import axios from 'axios;`
+  }
   getListServiceTemplate(url?: string): string {
-    return `import axios from 'axios'
-function getList(){
+    return `
+function list(){
   return new Promise((resolve: Function, reject: Function) => {
     axios({
-      url:' ${ url||'/project/list'} ',
+      url:'${ url||''}',
       method:'get'
     }).then((res: any) => {
       resolve(res.data.data.map((o: any ) =>  { return { value:o.id,text:o.projName} }))
@@ -24,11 +30,12 @@ function getList(){
   })
 }`
   }
-  getAddService(url?: string): string {
-    return `updateProject(data: any) {
+  getAddServiceTemplate(url?: string): string {
+    return `
+update(data: any) {
   return new Promise((resolve: Function,reject: Function) => {
     axios({
-      url:'/app/projectManage/updateSelective',
+      url:'${ url||''}',
       method:'post',
       data:qs.stringify(data)
     }).then((res: any) => {
@@ -38,8 +45,40 @@ function getList(){
     });
   })
 }`
-
   }
+  getUpdateServiceTemplate(url?: string): string {
+    return `
+update(data: any) {
+  return new Promise((resolve: Function,reject: Function) => {
+    axios({
+      url:'${ url||''}',
+      method:'post',
+      data:qs.stringify(data)
+    }).then((res: any) => {
+      resolve(res.data)
+    }).catch((err: any) => {
+      reject(err)
+    });
+  })
+}`
+  }
+  getDeleteServiceTemplate(url?: string): string {
+    return `
+delete(ids: any) {
+  return new Promise((resolve: Function, reject: Function) => {
+    axios({
+      url:'${ url||''}',
+      method:'get',
+      params:{ids:ids}
+    }).then((res: any) => {
+      resolve(res.data)
+    }).catch ((err) => {
+      reject(err)
+    })
+  })
+}`
+  }
+  
 }
 
 
